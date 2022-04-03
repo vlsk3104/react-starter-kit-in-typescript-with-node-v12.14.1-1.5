088ecd473731136139f7a68e5c8c6f7928256065
vlsk3104 <vlsk3104@gmail.com>
2022-04-03 19:26:53 +0900

    useRefのより実践的な使い方とOptional ChainingとNon-Null Assertion Operatorの紹介

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -9,10 +9,15 @@ const Counter: React.FC<{}> = () => {
   const decrement = () => {
     setValue((prevState) => prevState - 1);
   };
+
   const renderTimes = useRef<number>(0);
   useEffect(() => {
     renderTimes.current = renderTimes.current + 1;
   });
+  const ref = useRef<HTMLInputElement>(null!);
+  const focusInput = () => {
+    ref.current.focus();
+  };
 
   return (
     <div>
@@ -20,6 +25,8 @@ const Counter: React.FC<{}> = () => {
       <button onClick={increment}>+1</button>
       <button onClick={decrement}>-1</button>
       <div>This component was re-rendered {renderTimes.current} times!</div>
+      <input ref={ref} type="text" />
+      <button onClick={focusInput}>Click Me!</button>
     </div>
   );
 };
